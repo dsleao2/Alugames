94fface29594722c159d8f72d734083eaf5651d4
Daniele Santana <dssleao2@icloud.com>
2025-05-07 21:49:35 -0300

    Imprimir na tela quantidade de jogos alugados.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+let jogosAlugados = 0;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+
+
 function alterarStatus(id) {
 let gameClicado = document.getElementById (`game-${id}`);
 let imagem = gameClicado.querySelector('.dashboard__item__img');
@@ -11,10 +19,12 @@ if (imagem.classList.contains('dashboard__item__img--rented')) {
 imagem.classList.remove('dashboard__item__img--rented');
 botao.classList.remove('dashboard__item__button--return');
 botao.textContent = 'Alugar';
+jogosAlugados--;
 } else {
     imagem.classList.add('dashboard__item__img--rented');
     botao.textContent = 'Devolver';
     botao.classList.add('dashboard__item__button--return');
+    jogosAlugados++;
 }
 
 
