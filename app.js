38dcae886115ca6e56e0a913a1ffcf8c56bac255
Daniele Santana <151483117+dsleao2@users.noreply.github.com>
2025-05-07 21:45:34 -0300

    Adicionando uma confirmação antes de devolver o jogo

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ function alterarStatus(id) {
 let gameClicado = document.getElementById (`game-${id}`);
 let imagem = gameClicado.querySelector('.dashboard__item__img');
 let botao = gameClicado.querySelector ('.dashboard__item__button');
-
+let nomeJogo =gameClicado.querySelector('dashboard__item__name');
 
 
 if (imagem.classList.contains('dashboard__item__img--rented')) {
+    //Adicionando uma confirmação antes de devolver o jogo
+    if(confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}`))
 imagem.classList.remove('dashboard__item__img--rented');
 botao.classList.remove('dashboard__item__button--return');
 botao.textContent = 'Alugar';
